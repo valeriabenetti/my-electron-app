902992be29ba84e7d4a9bb1bb405d27632593811
Val <vbenetti46@gmail.com>
2024-08-25 13:34:55 -0400

    Init Electron App
    Initial App for Electron Tutorial

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('index.html')
+}
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+})
